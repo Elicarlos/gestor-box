f69138e577718ad6f89c353b2fde9fe6769795a9
Elicarlos Ferreira <elicarlosantos_@hotmail.com>
2025-06-19 16:35:52 -0300

    Add login API test and pytest configuration

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,12 @@
-from django.test import TestCase
+import os
+import django
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+django.setup()
+
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import AccessToken
 from .models import Funcionario
 
 
@@ -14,3 +20,23 @@ class FuncionarioManagerTests(TestCase):
         self.assertEqual(funcionario.user.username, "123")
         self.assertEqual(funcionario.user.first_name, "Teste")
         self.assertTrue(funcionario.user.check_password("pwd12345"))
+
+
+class LoginAPITests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.funcionario = Funcionario.objects.create_user(
+            matricula="321", nome="Login", senha="loginpwd"
+        )
+
+    def test_login_returns_jwt_token(self):
+        response = self.client.post(
+            "/api/login/",
+            {"matricula": "321", "senha": "loginpwd"},
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIn("access", data)
+        # validate that the token can be decoded
+        AccessToken(data["access"])
