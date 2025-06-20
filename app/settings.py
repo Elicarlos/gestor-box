f0516bd217ccd9d2ae64525bdbbf3b2db5eaed47
Elicarlos <elicarlosantos_@hotmail.com>
2025-06-20 08:57:17 -0300

    update fix

--- app/settings.py
+++ app/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', ''),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', ''),
         'USER': os.getenv('DB_USER', ''),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
