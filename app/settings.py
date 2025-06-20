7961720a4b1f8a050283f4d0003b0463fc9595c4
Elicarlos <elicarlosantos_@hotmail.com>
2025-06-20 08:32:59 -0300

    feat: sistem update

--- app/settings.py
+++ app/settings.py
@@ -82,18 +82,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': BASE_DIR / os.getenv('DB_NAME', 'db.sqlite3'),
-        # Para PostgreSQL, descomente e use as linhas abaixo:
-        # 'USER': os.getenv('DB_USER', ''),
-        # 'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        # 'HOST': os.getenv('DB_HOST', ''),
-        # 'PORT': os.getenv('DB_PORT', ''),
+        'ENGINE': os.getenv('DB_ENGINE', ''),
+        'NAME': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
 
